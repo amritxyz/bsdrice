83436b26dda109bde872ccb0da39325decd8a7f9
NyxVoid <amrit44404@proton.me>
2024-12-28 00:25:08 +0545

    new minor changes

--- .local/bin/net.sh
+++ .local/bin/net.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
-echo
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo 'vi /etc/hostname.iwm0'
-echo
-echo 'join "WIFI_NAME" wpakey "PASSWORD"'
-echo 'inet autoconf'
-echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-echo
+cat << "EOF"
+
+/****************************************************************
+ *								*
+ *				WIFI				*
+ *								*
+ *		   $ doas vi /etc/hostname.iwm0			*
+ *								*
+ *		| 1 join "WIFI_NAME" wpakey "PASSWORD		*
+ *		| 2 inet autoconf				*
+ *								*
+ *		$ doas sh /etc/netstart <interface>		*
+ *								*
+ ****************************************************************
+ *
+ *
+ ****************************************************************
+ *								*
+ *				DNS				*
+ *								*
+ *		    $ doas vi /etc/resolv.conf			*
+ *								*
+ *		| 1  search example.com				*
+ *		| 2  nameserver 1.1.1.1				*
+ *		| 2  nameserver 1.0.0.1				*
+ *								*
+ *		# making immutable(OPTIONAL)			*
+ *		$ doas chflags schg /etc/resolv.conf		*
+ *								*
+ *		$ dig @1.1.1.1 openbsd.org			*
+ *								*
+ *		$ doas tail -f /var/log/messages		*
+ *								*
+ ****************************************************************
+ */
+EOF
